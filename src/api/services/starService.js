94ba3997c6538b9814e64a0f62b43f500e230cab
Douglas Alves <douglas.expalves@gmail.com>
2018-01-22 04:12:01 -0200

    add star service

---
+++ src/api/services/starService.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+import { getFromUsername, setFromUsername } from './../../helpers/redis'
+
+// call the GitHub API to fetch information about the user's repositories
+function getUserRepositories(user) {
+  const githubEndpoint = 'https://api.github.com/users/' + user + '/repos' + '?per_page=100'
+  return axios.get(githubEndpoint)
+}
+
+// add up all the stars and return the total number of stars across all repositories
+function computeTotalStars(repositories) {
+  return repositories.data.reduce((prev, curr) => {
+    return prev + curr.stargazers_count
+  }, 0)
+}
+
+function getTotalStars(username) {
+  return new Promise((resolve, reject) => {
+    getFromUsername(username)
+      .then((data) => {
+        return resolve(data)
+      })
+      .catch(() => {
+        getUserRepositories(username)
+        .then(computeTotalStars)
+        .then((totalStars) => {
+          // store the key-value pair (username:totalStars) in our cache
+          // with an expiry of 1 minute (60s)
+          // return the result to the user
+
+          setFromUsername(username, totalStars)
+    
+          return resolve({ totalStars: totalStars, source: 'GitHub API', code: 200 })
+        }).catch((response) => {
+            return reject({ message: 'The GitHub username could not be found. Try "coligo-io" as an example!', code: response.status })
+        })
+      })
+  })
+}
+
+export { getTotalStars }
